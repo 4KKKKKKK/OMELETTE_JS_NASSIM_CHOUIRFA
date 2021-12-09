309902018b231aecd809828761d3768cd46254f6
Nass <nassim.chouirfa@gmail.comgit config user.namegit config --global user.email nassim.chouirfa@gmail.com>
2021-12-09 09:22:58 +0100

    fichier instances contenant les ingredients, panier, lieux et objet fini

--- public/module/class.js
+++ public/module/class.js
@@ -5,48 +5,8 @@ class Personne {
         this.argent = argent; //number
         this.mainDroite = []; //tableau
         this.mainGauche = [];
-
-        //methode se deplacer
-        this.seDeplacer = (dar, arrivee) => {
-            arrivee.personne.push(this);
-            dar.personne.splice(dar.personne.indexOf(this), 1);
-
-            this.lieu = arrivee;
-            console.log(`${this.nom} est actuellemement à la ${this.lieu.nom}`);
-        }
-
-        //methode pour prendre un panier
-        this.prendrePanier = (place) => {
-            if (place.nom == "Epicerie") {
-                this.mainDroite.push(place.sacPaniers.shift());
-                console.log(`${this.nom} a pris le ${this.mainDroite[1].type}`);
-            }
-        }
-
-        //methode pour mettre des ingrédients dans son panier
-        this.prendreIngredients = (place) => {
-            if (place.nom == "Epicerie") {
-                place.ingredients.forEach(e => {
-                    this.mainGauche[0].content.push(e);
-                    console.log(`${this.nom} a mis ${place.nom} dans son panier !`);
-                });
-            }
-        }
-
-        //mzthode pour acheter els ingrédients pris
-        this.payerArticle = (place) =>{
-            if (place.nom == "Epicerie") {
-                place.ingredients.forEach(e => {
-                    this.mainDroite[0].content.push(e);
-                    this.argent -= e.prix;
-                    console.log(`${this.nom} a acheter ${e.nom} au prix de : ${e.prix}€`);
-                });
-            }
-        }
     }
-
 }
-
 class Lieu {
     constructor(nom, personnes) {
         this.nom = nom;
---
+++ public/module/instances.js
@@ -0,0 +1,62 @@
+// Personnage
+
+let nassim = new Personne("Nassim", "nullePart", 100);
+
+// Lieu 
+
+let maison = new Lieu("Maison", []);
+let epicerie = new Lieu("Epicerie", []);
+
+epicerie.sacPaniers = [{
+        type: "panier Carré",
+        content: []
+    },
+    {
+        type: "panier Rond",
+        content: []
+    },
+    {
+        type: "panier Charrette",
+        content: []
+    },
+];
+
+
+// Ingrédients
+
+let oignon = new Ingredients("Oignon", "entier", 1);
+let oeuf = new Ingredients("Oeuf", "entier", 3);
+let sel = new Ingredients("Sel", "moulu", 1);
+let cumin = new Ingredients("Cumin", "moulu", 1);
+let fromage = new Ingredients("Fromage", "entier", 1);
+let eau = new Ingredients("Eau", "entier", 1);
+
+epicerie.ingredients = [oignon, oeuf, sel, cumin, fromage, eau];
+
+// Objets 
+
+let couteau = {
+    nom: "Couteau",
+    action: "coupé"
+}
+
+let poele = {
+    nom: "Poêle",
+    content: [],
+    cuir() {
+        setTimeout(() => {
+            this.content[0].etat = "cuit";
+        }, 4000)
+    }
+}
+
+let bol = {
+    nom: "Bol",
+    content: [],
+    melanger(nomMelange) {
+        let newMelange = {
+            nom: "nomMelange",
+            etat: "pas cuit"
+        }
+    }
+}
